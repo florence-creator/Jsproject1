7e28d72cd9c5d2eac8f739c1ea470951ad7f2068
Florence Nafula <flowanaswa@gmail.com>
2022-06-22 12:11:30 +0300

    Made few changes

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
-function bfunction() {
- let userInput = document.querySelector("#name");
- let message = document.querySelector("#message");
-
- message.innerHTML="Hello " + userInput.value + " welcome to the bootcamp";
-}
+const bfunction = () => {
+    let userInput = document.querySelector("#text");
+     const generatedElement = document.createElement('p');
+     generatedElement.innerHTML = `Hello ${userInput.value}, welcome to the bootcamp`;
+     document.body.appendChild(generatedElement);
+   }
